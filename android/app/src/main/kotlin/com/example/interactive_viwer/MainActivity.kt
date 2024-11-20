24372cda01046111f69c04165e96b84728af0f47
pratiksha <pratiksha10102001@gmail.com>
2024-11-20 09:51:23 +0530

    first change

---
+++ android/app/src/main/kotlin/com/example/interactive_viwer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.interactive_viwer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+
+void main() => runApp( const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeView(),
+    );
+  }
+}
+
+class HomeView extends StatefulWidget{
+  const HomeView({super.key});
+
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+       backgroundColor: Colors.black,
+
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          InteractiveViewer(
+            // maxScale: 25,
+            // minScale: 0.5,
+            child: Image.network("assets/cartoonimage.webp",height: double.infinity, width: 700,),
+
+          ),
+          Image.network(" assets/cartoonimage.webp", height: double.infinity, width: 700,),
+
+        ],
+
+      ),
+    );
+  }
+}
